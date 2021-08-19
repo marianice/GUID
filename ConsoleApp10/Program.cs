8cae3bef2c46b1267448a4672d34fef4a8f4e8fe
Maria Grivorieva <masha_21.02.99@mail.ru>
2021-08-19 13:08:40 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool existed;
+            // получаем GUID приложения
+            string guid = Marshal.GetTypeLibGuidForAssembly(Assembly.GetExecutingAssembly()).ToString();
+
+            Mutex mutexObj = new Mutex(true, guid, out existed);
+
+            if (existed)
+            {
+                Console.WriteLine("Приложение работает");
+            }
+            else
+            {
+                Console.WriteLine("Приложение уже было запущено. И сейчас оно будет закрыто.");
+                Thread.Sleep(3000);
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
